24d7ad30932e98686400b265877751a173c5c008
Julia <yulyakonoval1998@gmail.com>
2021-01-11 15:39:08 +0200

    1. API was created

---
+++ src/data.js
@@ -0,0 +1,160 @@
+import { v4 as uuidv4 } from "uuid";
+
+function showQuote() {
+  return [
+    {
+      body:
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      author: "- Nelson Mandela",
+      color: "#F3DCD4",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "The way to get started is to quit talking and begin doing.",
+      author: "- Walt Disney",
+      color: "#D9E3DA",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+      author: "- Steve Jobs",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "When you reach the end of your rope, tie a knot in it and hang on.",
+      author: "- Franklin D. Roosevelt",
+      color: "#ECC9C7",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "If life were predictable it would cease to be life, and be without flavor.",
+      author: "- Eleanor Roosevelt",
+      color: "#F3DCD4",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+      author: "- Benjamin Franklin",
+      color: "#D9E3DA",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "It is during our darkest moments that we must focus to see the light.",
+      author: "- Aristotle",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+      author: "- Mother Teresa",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "In the end, it's not the years in your life that count. It's the life in your years.",
+      author: "- Abraham Lincoln",
+      color: "#ECC9C7",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "Life is never fair, and perhaps it is a good thing for most of us that it is not.",
+      author: "- Oscar Wilde",
+      color: "#F3DCD4",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "The purpose of our lives is to be happy.",
+      author: "- Dalai Lama",
+      color: "#D9E3DA",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "Life is really simple, but we insist on making it complicated.",
+      author: "- Confucius",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "Love the life you live. Live the life you love.",
+      author: " - Bob Marley",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+      author: "- Winston S. Churchill",
+      color: "#ECC9C7",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "If you really look closely, most overnight successes took a long time.",
+      author: "- Steve Jobs",
+      color: "#F3DCD4",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "The secret of success is to do the common thing uncommonly well.",
+      author: "- D. Rockefeller Jr.",
+      color: "#D9E3DA",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body:
+        "The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
+      author: "- Barack Obama",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "The way to get started is to quit talking and begin doing.",
+      author: "- Walt Disney",
+      color: "#D1CFC0",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "I never dreamed about success, I worked for it.",
+      author: "- Estee Lauder",
+      color: "#ECC9C7",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      body: "Try not to become a man of success. Rather become a man of value.",
+      author: " - Albert Einstein",
+      color: "#F3DCD4",
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default showQuote;
