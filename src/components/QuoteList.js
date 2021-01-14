ca1c0ddf285f7cc97742c04d528bd01bc417ece7
Julia <yulyakonoval1998@gmail.com>
2021-01-14 17:33:45 +0200

    the application was styled

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -6,11 +6,9 @@ import "./QuoteStyle.css";
 const QuoteList = () => {
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState([]);
-  const [backgroundCol, setBackgroundCol] = useState();
+  const [backgroundCol, setBackgroundCol] = useState("");
 
-  let colors = [
-  "#F3DCD4", "#D9E3DA", "#D1CFC0", "#ECC9C7",
-  ]
+  let colors = ["#F3DCD4", "#D9E3DA", "#D1CFC0", "#ECC9C7"];
   useEffect(() => {
     async function fetchData() {
       try {
@@ -30,14 +28,22 @@ const QuoteList = () => {
   }
   function generateColor() {
     let randomColor = Math.floor(Math.random() * colors.length);
-    setBackgroundCol(randomColor);
+    setBackgroundCol(colors[randomColor]);
+  }
+  function handleClick() {
+    generatePrediction();
+    generateColor();
   }
   return (
-    <div className="container" generateColor={generateColor} style={{backgroundColor: backgroundCol}}>
-    <div className="prediction-container">
-      <Quote randomQuote={randomQuote} />
-      <button onClick={generatePrediction}>Generate Prediction</button>
-    </div>
+    <div
+      className="container"
+      generateColor={generateColor}
+      style={{ backgroundColor: backgroundCol }}
+    >
+      <div className="prediction-container">
+        <Quote randomQuote={randomQuote} />
+        <button className="btn" onClick={handleClick}>Generate Prediction</button>
+      </div>
     </div>
   );
 };
