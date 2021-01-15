959c7a5015c67d4778957d8de1354f9e0306d5ca
Julia <yulyakonoval1998@gmail.com>
2021-01-15 12:02:17 +0200

    default text applied

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -7,7 +7,7 @@ const Quote = ({ randomQuote }) => {
   return (
     <div className="prediction-container">
       <h1 className="header">Your prediction for today:</h1>
-      <h1 className="prediction-body">'{text}'</h1>
+      {text === "" ? <p>click for prediction</p> : <p>{text}</p>}
       <h2 className="author">{author}</h2>
     </div>
   );
